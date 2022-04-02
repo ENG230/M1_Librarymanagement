896f34f345bedb93aedd91049635b9456811761c
ENG230 <engilearunkumar77@gmail.com>
2022-04-02 22:12:33 +0530

    updated

--- 3_Implementation/test/test_library.c
+++ 3_Implementation/test/test_library.c
@@ -1,43 +1,44 @@
-#include"library_Managment_system.h"
+#include"Library_management.h"
 #include"unity_internals.h"
 #include"unity.h"
 
-void setUp(){}
+void setUp(){
 
-void tearDown(){}
-
-
-void test_addition(void){
-    TEST_ASSERT_EQUAL(pass,addition(125, "Sonika", "Dragon"));
 }
 
-void test_viewbook(void){
-    TEST_ASSERT_EQUAL(pass,viewbook());
-}
+void tearDown(){
 
-void test_searchbook(void){
-    TEST_ASSERT_EQUAL(fail,searchbook(-2));
-    TEST_ASSERT_EQUAL(pass,searchbook(125));
 }
 
 
+void test_ToAddingBook(void){
+    TEST_ASSERT_EQUAL(pass,(409, "ARUN", "PYTHON"));
+}
 
-void test_deletebook(void){
-    TEST_ASSERT_EQUAL(pass,deletebook(125));
+void test_ToViewBook(void){
+    TEST_ASSERT_EQUAL(pass,ToViewBook());
+}
+
+void test_ToSearchingBook(void){
+    TEST_ASSERT_EQUAL(fail,ToSearchingBook(-10));
+    TEST_ASSERT_EQUAL(pass,ToSearchingBook(409));
+}
+
+
+
+void test_ToDeletingBook(void){
+    TEST_ASSERT_EQUAL(pass,ToDeletingBook(409));
 }        
 
-void test_issuebook(void){
-    TEST_ASSERT_EQUAL(pass,issuebook(125));
-}
+
 
 int main(){
     UNITY_BEGIN();
 
-    RUN_TEST(test_addition);
-    RUN_TEST(test_searchbook);
-    RUN_TEST(test_issuebook);
-    RUN_TEST(test_deletebook);
-    RUN_TEST(test_viewbook);
+    RUN_TEST(test_ToAddingBook);
+    RUN_TEST(test_ToSearchingBook);
+    RUN_TEST(test_ToDeletingBook);
+    RUN_TEST(test_ToViewBook);
     
     return UNITY_END();
 }
